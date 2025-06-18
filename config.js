a485f71790b0ec7227192a534098180634560895
Richard Rodriguez <richard.fortuito@gmail.com>
2025-06-18 08:05:02 +0800

    Initial commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
-//const BASE_URL = 'https://crmapi-production-cab2.up.railway.app';
+const BASE_URL = 'https://crmapi-production-cab2.up.railway.app';
 
-const BASE_URL = 'http://192.168.254.101:3000';
+//const BASE_URL = 'http://192.168.254.101:3000';
 
 export default BASE_URL;
